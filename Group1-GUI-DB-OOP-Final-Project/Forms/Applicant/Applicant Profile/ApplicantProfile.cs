61f751a77feac3b8c5e16a2ec581af745d9031ca
Zelycx <ezekielmores084@gmail.com>
2026-06-07 22:56:22 +0800

    Reorganize project structure, add forms, and fix .gitignore

---
+++ Group1-GUI-DB-OOP-Final-Project/Forms/Applicant/Applicant Profile/ApplicantProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Group1_GUI_DB_OOP_Final_Project.Forms.Applicant
+{
+    public partial class ApplicantProfile : Form
+    {
+        public ApplicantProfile()
+        {
+            InitializeComponent();
+        }
+    }
+}
