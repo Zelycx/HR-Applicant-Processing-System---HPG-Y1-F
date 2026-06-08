6cc47885124ffa0607cb6b86fac13560f91b0e49
migiee <adelanteianmiguel@gmail.com>
2026-06-08 15:40:59 +0800

    added HR APPLICANT LIST

---
+++ HRApplicant system/Forms/HRApplicantList.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace HRApplicant_system.Forms
+{
+    partial class ApplicantList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.lblFilter = new System.Windows.Forms.Label();
+            this.cmbFilter = new System.Windows.Forms.ComboBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lvApplicants = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnReview = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lblTitle.Location = new System.Drawing.Point(237, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(245, 40);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Applicant List";
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(12, 84);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(64, 20);
+            this.lblSearch.TabIndex = 2;
+            this.lblSearch.Text = "Search:";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(82, 78);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(250, 26);
+            this.txtSearch.TabIndex = 3;
+            // 
+            // lblFilter
+            // 
+            this.lblFilter.AutoSize = true;
+            this.lblFilter.Location = new System.Drawing.Point(385, 84);
+            this.lblFilter.Name = "lblFilter";
+            this.lblFilter.Size = new System.Drawing.Size(60, 20);
+            this.lblFilter.TabIndex = 4;
+            this.lblFilter.Text = "Status:";
+            // 
+            // cmbFilter
+            // 
+            this.cmbFilter.FormattingEnabled = true;
+            this.cmbFilter.Items.AddRange(new object[] {
+            "All",
+            "Draft",
+            "Submitted",
+            "Under Review",
+            "Shortlisted",
+            "For Interview",
+            "Accepted",
+            "Rejected"});
+            this.cmbFilter.Location = new System.Drawing.Point(451, 84);
+            this.cmbFilter.Name = "cmbFilter";
+            this.cmbFilter.Size = new System.Drawing.Size(150, 28);
+            this.cmbFilter.TabIndex = 5;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnSearch.ForeColor = System.Drawing.Color.White;
+            this.btnSearch.Location = new System.Drawing.Point(16, 128);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(94, 36);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(132, 128);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(84, 36);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // lvApplicants
+            // 
+            this.lvApplicants.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.lvApplicants.FullRowSelect = true;
+            this.lvApplicants.GridLines = true;
+            this.lvApplicants.HideSelection = false;
+            this.lvApplicants.Location = new System.Drawing.Point(16, 186);
+            this.lvApplicants.Name = "lvApplicants";
+            this.lvApplicants.Size = new System.Drawing.Size(740, 350);
+            this.lvApplicants.TabIndex = 8;
+            this.lvApplicants.UseCompatibleStateImageBehavior = false;
+            this.lvApplicants.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "App ID";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Full Name";
+            this.columnHeader2.Width = 180;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Job Position";
+            this.columnHeader3.Width = 180;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Date Applied";
+            this.columnHeader4.Width = 120;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Status";
+            this.columnHeader5.Width = 120;
+            // 
+            // btnReview
+            // 
+            this.btnReview.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnReview.ForeColor = System.Drawing.Color.White;
+            this.btnReview.Location = new System.Drawing.Point(16, 554);
+            this.btnReview.Name = "btnReview";
+            this.btnReview.Size = new System.Drawing.Size(181, 36);
+            this.btnReview.TabIndex = 9;
+            this.btnReview.Text = "Review Applicant";
+            this.btnReview.UseVisualStyleBackColor = false;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(272, 554);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(90, 36);
+            this.btnBack.TabIndex = 10;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // HRApplicantList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 616);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnReview);
+            this.Controls.Add(this.lvApplicants);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.cmbFilter);
+            this.Controls.Add(this.lblFilter);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "HRApplicantList";
+            this.Text = "HRApplicantList";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label lblFilter;
+        private System.Windows.Forms.ComboBox cmbFilter;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ListView lvApplicants;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Button btnReview;
+        private System.Windows.Forms.Button btnBack;
+    }
+}
---
+++ HRApplicant system/Forms/HRApplicantList.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HRApplicant_system.Forms
+{
+    public partial class ApplicantList : Form
+    {
+        public ApplicantList()
+        {
+            InitializeComponent();
+        }
+
+        private void ApplicantList_Load(object sender, EventArgs e)
+        {
+            cmbStatus.Items.Clear();
+            cmbStatus.Items.Add("All");
+            cmbStatus.Items.Add("Draft");
+            cmbStatus.Items.Add("Submitted");
+            cmbStatus.Items.Add("Under Review");
+            cmbStatus.Items.Add("Shortlisted");
+            cmbStatus.Items.Add("For Interview");
+            cmbStatus.Items.Add("For Final Review");
+            cmbStatus.Items.Add("Accepted");
+            cmbStatus.Items.Add("Rejected");
+            cmbStatus.SelectedIndex = 0;
+
+            // Setup DataGridView columns manually since no DB yet
+            dgvApplicants.Columns.Clear();
+            dgvApplicants.Columns.Add("AppID", "App ID");
+            dgvApplicants.Columns.Add("FullName", "Full Name");
+            dgvApplicants.Columns.Add("JobPosition", "Job Position");
+            dgvApplicants.Columns.Add("DateApplied", "Date Applied");
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Search will work once DB is connected.", "Info",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtSearch.Clear();
+            cmbStatus.SelectedIndex = 0;
+        }
+
+        private void btnReviewApplicant_Click(object sender, EventArgs e)
+        {
+            if (dgvApplicants.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Please select an applicant first.", "No Selection",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            ApplicantReview form = new ApplicantReview();
+            form.Show();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
