0e58d2c72df8ac4dccb547ebc16691b23731ae99
migiee <adelanteianmiguel@gmail.com>
2026-06-08 16:06:11 +0800

    added HR APPLICANT REVIEW

---
+++ HRApplicant system/Forms/HRapplicantReview.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace HRApplicant_system.Forms
+{
+    partial class HRapplicantReview
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblJob = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblContact = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblRemarks = new System.Windows.Forms.Label();
+            this.rtbRemarks = new System.Windows.Forms.RichTextBox();
+            this.btnStartReview = new System.Windows.Forms.Button();
+            this.btnShortlist = new System.Windows.Forms.Button();
+            this.btnReject = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.lblNameValue = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lblTitle.Location = new System.Drawing.Point(108, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(309, 40);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Applicant Review";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(12, 68);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(171, 25);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Applicant Name:";
+            // 
+            // lblJob
+            // 
+            this.lblJob.AutoSize = true;
+            this.lblJob.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJob.Location = new System.Drawing.Point(12, 110);
+            this.lblJob.Name = "lblJob";
+            this.lblJob.Size = new System.Drawing.Size(175, 25);
+            this.lblJob.TabIndex = 4;
+            this.lblJob.Text = "Applied Position:";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(12, 151);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(159, 25);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "Current Status:";
+            // 
+            // lblContact
+            // 
+            this.lblContact.AutoSize = true;
+            this.lblContact.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContact.Location = new System.Drawing.Point(12, 187);
+            this.lblContact.Name = "lblContact";
+            this.lblContact.Size = new System.Drawing.Size(94, 25);
+            this.lblContact.TabIndex = 8;
+            this.lblContact.Text = "Contact:";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAddress.Location = new System.Drawing.Point(12, 223);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(99, 25);
+            this.lblAddress.TabIndex = 10;
+            this.lblAddress.Text = "Address:";
+            // 
+            // lblRemarks
+            // 
+            this.lblRemarks.AutoSize = true;
+            this.lblRemarks.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRemarks.Location = new System.Drawing.Point(12, 260);
+            this.lblRemarks.Name = "lblRemarks";
+            this.lblRemarks.Size = new System.Drawing.Size(138, 25);
+            this.lblRemarks.TabIndex = 12;
+            this.lblRemarks.Text = "HR Remarks:";
+            // 
+            // rtbRemarks
+            // 
+            this.rtbRemarks.Location = new System.Drawing.Point(12, 297);
+            this.rtbRemarks.Name = "rtbRemarks";
+            this.rtbRemarks.Size = new System.Drawing.Size(500, 80);
+            this.rtbRemarks.TabIndex = 13;
+            this.rtbRemarks.Text = "";
+            // 
+            // btnStartReview
+            // 
+            this.btnStartReview.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnStartReview.ForeColor = System.Drawing.Color.White;
+            this.btnStartReview.Location = new System.Drawing.Point(17, 393);
+            this.btnStartReview.Name = "btnStartReview";
+            this.btnStartReview.Size = new System.Drawing.Size(166, 38);
+            this.btnStartReview.TabIndex = 14;
+            this.btnStartReview.Text = "Start Review";
+            this.btnStartReview.UseVisualStyleBackColor = false;
+            // 
+            // btnShortlist
+            // 
+            this.btnShortlist.Location = new System.Drawing.Point(19, 437);
+            this.btnShortlist.Name = "btnShortlist";
+            this.btnShortlist.Size = new System.Drawing.Size(164, 30);
+            this.btnShortlist.TabIndex = 15;
+            this.btnShortlist.Text = "Shortlist Applicant";
+            this.btnShortlist.UseVisualStyleBackColor = true;
+            // 
+            // btnReject
+            // 
+            this.btnReject.BackColor = System.Drawing.Color.Red;
+            this.btnReject.ForeColor = System.Drawing.Color.White;
+            this.btnReject.Location = new System.Drawing.Point(23, 473);
+            this.btnReject.Name = "btnReject";
+            this.btnReject.Size = new System.Drawing.Size(164, 35);
+            this.btnReject.TabIndex = 16;
+            this.btnReject.Text = "Reject Applicant";
+            this.btnReject.UseVisualStyleBackColor = false;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(426, 473);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(86, 29);
+            this.btnBack.TabIndex = 17;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // lblNameValue
+            // 
+            this.lblNameValue.AutoSize = true;
+            this.lblNameValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNameValue.Location = new System.Drawing.Point(198, 68);
+            this.lblNameValue.Name = "lblNameValue";
+            this.lblNameValue.Size = new System.Drawing.Size(19, 25);
+            this.lblNameValue.TabIndex = 18;
+            this.lblNameValue.Text = "-";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(198, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 25);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "-";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(198, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 25);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "-";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(198, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 25);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "-";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(198, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 25);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "-";
+            // 
+            // HRapplicantReview
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 544);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblNameValue);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnReject);
+            this.Controls.Add(this.btnShortlist);
+            this.Controls.Add(this.btnStartReview);
+            this.Controls.Add(this.rtbRemarks);
+            this.Controls.Add(this.lblRemarks);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblContact);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblJob);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "HRapplicantReview";
+            this.Text = "HRapplicantReview";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblJob;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblContact;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label lblRemarks;
+        private System.Windows.Forms.RichTextBox rtbRemarks;
+        private System.Windows.Forms.Button btnStartReview;
+        private System.Windows.Forms.Button btnShortlist;
+        private System.Windows.Forms.Button btnReject;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Label lblNameValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ HRApplicant system/Forms/HRapplicantReview.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HRApplicant_system.Forms
+{
+    public partial class ApplicantReview : Form
+    {
+        public ApplicantReview()
+        {
+            InitializeComponent();
+        }
+
+        private void ApplicantReview_Load(object sender, EventArgs e)
+        {
+            // Buttons enabled by default for UI testing
+            btnStartReview.Enabled = true;
+            btnShortlistApplicant.Enabled = false;
+            btnRejectApplicant.Enabled = true;
+        }
+
+        private void btnStartReview_Click(object sender, EventArgs e)
+        {
+            lblCurrentStatus.Text = "Under Review";
+            btnStartReview.Enabled = false;
+            btnShortlistApplicant.Enabled = true;
+
+            MessageBox.Show("Application is now Under Review.", "Status Updated",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnShortlistApplicant_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtHRRemarks.Text))
+            {
+                MessageBox.Show("Please enter HR Remarks before shortlisting.", "Remarks Required",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            lblCurrentStatus.Text = "Shortlisted";
+            btnShortlistApplicant.Enabled = false;
+            btnRejectApplicant.Enabled = false;
+
+            MessageBox.Show("Applicant has been Shortlisted.", "Status Updated",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnRejectApplicant_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtHRRemarks.Text))
+            {
+                MessageBox.Show("Please enter HR Remarks before rejecting.", "Remarks Required",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            DialogResult confirm = MessageBox.Show(
+                "Are you sure you want to reject this applicant?",
+                "Confirm Rejection",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning);
+
+            if (confirm == DialogResult.Yes)
+            {
+                lblCurrentStatus.Text = "Rejected";
+                btnStartReview.Enabled = false;
+                btnShortlistApplicant.Enabled = false;
+                btnRejectApplicant.Enabled = false;
+
+                MessageBox.Show("Applicant has been Rejected.", "Status Updated",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
