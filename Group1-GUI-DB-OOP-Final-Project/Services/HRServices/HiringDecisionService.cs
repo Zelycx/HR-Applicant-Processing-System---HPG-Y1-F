cf39ff17142e837ce8f7a1515d786d6f83f11de6
Bruce <ambayecbruceandrew@gmail.com>
2026-06-23 19:31:46 +0800

    Hiring Decision Module - Stable Version (Without Audit Trail)

---
+++ Group1-GUI-DB-OOP-Final-Project/Services/HRServices/HiringDecisionService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group1_GUI_DB_OOP_Final_Project.Services.HRServices
+{
+    internal class HiringDecisionService
+    {
+    }
+}
